904d57c25ae294c651176b4804fb0cbc52e6cd52
WillAdebisi <152137039+WillAdebisi@users.noreply.github.com>
2024-05-16 02:02:56 -0500

    right and left side completed

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
-import './Hero.css'
-import Header from '../Header/Header'
+import React from 'react';
+import './Hero.css';
+import Header from '../Header/Header';
+import hero_image from "../../assets/hero_image.png"
+import hero_image_back from "../../assets/hero_image_back.png"
+import Heart from "../../assets/heart.png"
+import Calories from "../../assets/calories.png"
 const Hero = () => {
   return (
     <div className="hero">
@@ -51,7 +55,27 @@ const Hero = () => {
         </div>
       </div>
       
-      <div className="right-h">right side</div>
+      <div className="right-h">
+        <button className='btn'>Join Now</button>
+
+
+        <div className="heart-rate">
+          <img src={Heart} alt="" />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </div>
+        {/* Hero Images */}
+        <img src={hero_image} alt="" className='hero-image'/>
+        <img src={hero_image_back} alt="" className='hero-image-back'/>
+        {/* Calories */}
+        <div className="calories">
+          <img src={Calories} alt="" />
+          <div>
+            <span>Calories Burned</span>
+            <span>220 kcal</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
