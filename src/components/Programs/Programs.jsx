f4208888fb90f3700ecbbceda4122e1241d8da5e
WillAdebisi <152137039+WillAdebisi@users.noreply.github.com>
2024-05-16 02:39:22 -0500

    Program Section Complete.

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Programs.css'
-import '../../data/programsData'
+import {programsData} from '../../data/programsData'
+import RightArrow from '../../assets/rightArrow.png'
 const Programs = () => {
   return (
     <div className='Programs' id='programs'>
@@ -10,8 +11,21 @@ const Programs = () => {
             <span>Programs</span>
             <span className='stroke-text'>to shape you</span>
         </div>
+        <div className="program-categories">
+            {programsData.map((program)=>(
+                <div className='category'>
+                    {program.image}
+                    <span>{program.heading}</span>
+                    <span>{program.details}</span>
+                    <div className="join-now">
+                        <span>Join Now</span>
+                        <img src={RightArrow} alt="" />
+                    </div>
+                </div>
+            ))}
+        </div>
     </div>
-  )
-}
+  );
+};
 
 export default Programs
